3d35bb61249fff66d7734c6788d07e6c07e87252
lilylinxl <1227495160@qq.com>
2020-09-06 11:02:57 +0800

    头部尾部样式完

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,15 +1,17 @@
 import React, { memo } from 'react';
 import { NavLink } from 'react-router-dom';
+import { SearchOutlined } from '@ant-design/icons'
+import { Input } from 'antd';
 
 import { headerLinks } from '@/common/local-data';
 
-import { HeaderWrapper,HeaderLeft,HeaderRight } from './style'
+import { HeaderWrapper, HeaderLeft, HeaderRight } from './style'
 
 export default memo(function XLAppHeader() {
 
     //页面代码
     const showSelectItem = (item, index) => {
-        if(index < 3) {
+        if (index < 3) {
             return (
                 <NavLink to={item.link} exact>
                     {item.title}
@@ -28,21 +30,23 @@ export default memo(function XLAppHeader() {
                     <a href="#/" className="logo sprite_01">网易云音乐</a>
                     <div className="select-list">
                         {
-                            headerLinks.map((item,index) =>　{
+                            headerLinks.map((item, index) => {
                                 return (
                                     <div key={item.title} className="select-item">
-                                        {showSelectItem(item,index)}
+                                        {showSelectItem(item, index)}
                                     </div>
                                 )
                             })
                         }
                     </div>
                 </HeaderLeft>
-                <HeaderRight>right</HeaderRight>
+                <HeaderRight>
+                    <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />} />
+                    <div className="center">创作者中心</div>
+                    <div>登录</div>
+                </HeaderRight>
             </div>
-            <div className="divider">
-                
-            </div>
+            <div className="divider"></div>
         </HeaderWrapper>
     )
 })
