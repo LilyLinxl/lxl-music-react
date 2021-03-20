b26568231f511f6869f61fad23a41d7b90652893
lilylinxl <1227495160@qq.com>
2021-03-20 11:35:02 +0800

    feat:theme-header-rcm组件的封装

---
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+
+import {
+    HotRecommendWrapper
+} from './style'
+import HYThemeHeaderRCM from '@/components/theme-header-rcm';
+
+export default memo(function LXLHotRecommend() {
+    return (
+        <HotRecommendWrapper>
+            <HYThemeHeaderRCM title="热门推荐" keywords={["华语", "流行", "民谣", "摇滚", "电子"]} />
+        </HotRecommendWrapper>
+    )
+})
--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,10 +1,22 @@
 import React, { memo, useEffect } from 'react'
-import { RecommendWrapper } from './style'
+import { RecommendWrapper,Content,RecommendLeft,RecommendRight } from './style'
 import LXLTopBanner from './c-cpns/top-banner'
+import LXLHotRecommend from './c-cpns/hot-recommend'
+import LXLRecommendRanking from './c-cpns/recommend-ranking'
+import LXLNewAlbum from './c-cpns/new-album'
+
 function XLRecommend() {
     return (
         <RecommendWrapper>
             <LXLTopBanner/>
+            <Content>
+                <RecommendLeft className="wrap-v2">
+                    <LXLHotRecommend/>
+                    <LXLRecommendRanking/>
+                    <LXLNewAlbum/>
+                </RecommendLeft>
+                <RecommendRight></RecommendRight>
+            </Content>
         </RecommendWrapper>
     )
 }
